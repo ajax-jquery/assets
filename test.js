05c7cfe85ff896eedb6b2ec9caca8c2a8606dbc3
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2025-02-21 03:14:29 +0800

    Update and rename test.js to test1.js

--- test.js
+++
@@ -1,3 +0,0 @@
-const scriptURL = new URL(document.currentScript.src);
-const scriptDir = scriptURL.origin + scriptURL.pathname.substring(0, scriptURL.pathname.lastIndexOf("/") + 1);
-console.log(scriptDir);
