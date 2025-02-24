bf61e83a1beda0769e264ba0fe1fd99194c33e01
ajax-jquery <fkansaz@gmail.com>
2025-02-24 15:29:57 +0800

    Tambah banyak file ke branch v44.2.0

---
+++ 44.2.0/ckeditor5-premium-features/translations/translation.umd.js
@@ -0,0 +1,23 @@
+/*
+ *             Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ *
+ *
+ *
+ *
+ *          +---------------------------------------------------------------------------------+
+ *          |                                                                                 |
+ *          |                                 Hello stranger!                                 |
+ *          |                                                                                 |
+ *          |                                                                                 |
+ *          |   What you're currently looking at is the source code of a legally protected,   |
+ *          |    proprietary software. Any attempts to deobfuscate / disassemble this code    |
+ *          |               are forbidden and will result in legal consequences.              |
+ *          |                                                                                 |
+ *          |                                                                                 |
+ *          +---------------------------------------------------------------------------------+
+ *
+ *
+ *
+ *
+ */
+(_0x99d014=>{const {translation:{dictionary:_0x12b8a5,getPluralForm:_0x5b175c}}={'translation':{'dictionary':{},'getPluralForm':null}};_0x99d014['translation']||={'dictionary':{},'getPluralForm':null},_0x99d014['translation']['dictionary']=Object['assign'](_0x99d014['translation']['dictionary'],_0x12b8a5),_0x99d014['translation']['getPluralForm']=_0x5b175c;})(window['CKEDITOR_TRANSLATIONS']||={});
---
+++ 44.2.0/index.js
@@ -0,0 +1,11 @@
+require("./ckeditor5/ckeditor5.css");
+require("./ckeditor5/ckeditor5.js");
+require("./ckeditor5/ckeditor5.min.js");
+require("./ckeditor5/ckeditor5.umd.js");
+require("./ckeditor5/ckeditor5.umd.min.js");
+require("./ckeditor5-premium-features/ckeditor5-premium-features.css");
+require("./ckeditor5-premium-features/ckeditor5-premium-features.js");
+require("./ckeditor5-premium-features/ckeditor5-premium-features.min.js");
+require("./ckeditor5-premium-features/ckeditor5-premium-features.umd.js");
+require("./ckeditor5-premium-features/ckeditor5-premium-features.umd.min.js");
+
