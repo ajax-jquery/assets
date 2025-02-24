43f80b91b2d56831150d828461c16ceee4b7555c
ajax-jquery <fkansaz@gmail.com>
2025-02-24 15:23:36 +0800

    Tambah banyak file ke branch v44.2.1

---
+++ 44.2.1/ckeditor5-premium-features/translations/translation.umd.js
@@ -0,0 +1,23 @@
+/*
+ *             Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ *
+ *
+ *
+ *
+ *          +---------------------------------------------------------------------------------+
+ *          |                                                                                 |
+ *          |                                 Hello stranger!                                 |
+ *          |                                                                                 |
+ *          |                                                                                 |
+ *          |   What you're currently looking at is the source code of a legally protected,   |
+ *          |    proprietary software. Any attempts to deobfuscate / disassemble this code    |
+ *          |               are forbidden and will result in legal consequences.              |
+ *          |                                                                                 |
+ *          |                                                                                 |
+ *          +---------------------------------------------------------------------------------+
+ *
+ *
+ *
+ *
+ */
+(_0x5c4ea4=>{const {translation:{dictionary:_0x4ba5fd,getPluralForm:_0x7484c2}}={'translation':{'dictionary':{},'getPluralForm':null}};_0x5c4ea4['translation']||={'dictionary':{},'getPluralForm':null},_0x5c4ea4['translation']['dictionary']=Object['assign'](_0x5c4ea4['translation']['dictionary'],_0x4ba5fd),_0x5c4ea4['translation']['getPluralForm']=_0x7484c2;})(window['CKEDITOR_TRANSLATIONS']||={});
